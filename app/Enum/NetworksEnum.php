a1e0c252b40a69870bb4b4fc5eab3285908c8434
dragomirt <dragomirt22@gmail.com>
2022-06-15 18:26:46 +0300

    adds models and migrations

---
+++ app/Enum/NetworksEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enum;
+
+enum NetworksEnum: string
+{
+    case FACEBOOK = "facebook";
+    case TWITTER = "twitter";
+}
---
+++ database/migrations/2022_06_15_151752_create_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('person_id');
+            $table->string('network');
+            $table->string('handle');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
